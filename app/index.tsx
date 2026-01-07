4d07b46b1beca0c04c772c4f6a0d73c4fa3cf192
Vinod663 <vinodfernando048@gmail.com>
2026-01-07 09:46:24 +0530

    Initial commit

---
+++ app/index.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from "react-native"
+import React from "react"
+import "../global.css"
+import { Redirect } from "expo-router"
+
+const Index = () => {
+  return <Redirect href="/login" />
+}
+
+export default Index
